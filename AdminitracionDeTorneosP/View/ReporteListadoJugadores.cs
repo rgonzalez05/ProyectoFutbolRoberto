bbdea32cc0f0f732bca169bdc86af727c53c797b
Debora Chacach <vasty0295@gmail.com>
2021-06-09 15:33:48 -0600

    reporte Debora

---
+++ AdminitracionDeTorneosP/View/ReporteListadoJugadores.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AdminitracionDeTorneosP.View
+{
+    public partial class ReporteListadoJugadores : Form
+    {
+        public ReporteListadoJugadores()
+        {
+            InitializeComponent();
+        }
+    }
+}
